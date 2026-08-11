e1a03a97411df32c85245a22ff26c9fe8ffd37af
Tiogaplanet <samuel.trassare@gmail.com>
2026-08-11 10:47:52 -0400

    Fix debug print format and flag reference

--- src/MPU_Shake.cpp
+++ src/MPU_Shake.cpp
@@ -12,13 +12,14 @@
  * with the License. You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  */
+#include "MPU_Shake.h"
 #include "MiP_Power_Up_-_D1_mini.h"
 
 // Implement the constructor to store the MiP reference.
 MiP_Shake::MiP_Shake(MiP& mip) : m_mip(mip) {}
 
 bool MiP_Shake::read() {
-  MIP_DEBUG_INFO_PRINTLN("MiP->Shake->read()");
+  MIP_DEBUG_INFO_PRINTLN(F("MiP->Shake->read()"));
   // Fetch bytes from the Serial receive buffer and process any event data found
   // within.
   m_mip.serial.processAllResponseData();
@@ -26,7 +27,7 @@ bool MiP_Shake::read() {
   if (m_mip.m_flags & MiP::MIP_FLAG_SHAKE_DETECTED) {
     // A shake event has been received since the last call to this function.
     // Return true and clear the shake detected bit.
-    m_mip.m_flags &= ~m_mip.MIP_FLAG_SHAKE_DETECTED;
+    m_mip.m_flags &= ~MiP::MIP_FLAG_SHAKE_DETECTED;
     return true;
   }
   return false;
