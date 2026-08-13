4a3eab0f2fb740bc553f830e8cec80f854b3dfa2
Tiogaplanet <samuel.trassare@gmail.com>
2026-08-13 10:19:54 -0400

    Change license from Apache 2.0 to MIT
    Updated license information from Apache 2.0 to MIT.

--- src/MPU_Debug.h
+++ src/MPU_Debug.h
@@ -10,10 +10,10 @@
  * @author Joao Lopes (Original Author)
  * @author Samuel Trassare (Maintainer)
  * @copyright Copyright (C) 2018-2026 Samuel Trassare
- * (https://github.com/Tiogaplanet) Licensed under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
+ * (https://github.com/Tiogaplanet) Licensed under the MIT License
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * https://opensource.org/licenses/MIT
  */
 #ifndef MPU_DEBUG_H
 #define MPU_DEBUG_H
